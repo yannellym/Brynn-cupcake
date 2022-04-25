b804e7f32da9d38b3366a0caa40a4035a0817dc1
yannelly mercado <yannellym@gmail.com>
2022-04-25 18:49:50 -0400

    new admin page

---
+++ pages/admin/login.jsx
@@ -0,0 +1,53 @@
+
+import { useRouter } from "next/router";
+import { useState } from "react";
+import styles from "../../styles/Login.module.css"
+
+
+
+const Login = () => {
+    const [username, setUsername] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [error, setError] = useState(false);
+    const router = useRouter();
+
+        const handleClick = async (event) => {
+            try{
+                await axios.post("http://localhost:3000/api/login", {
+                    username, 
+                    password,
+                });
+                router("/admin")
+            }catch(err){
+                console.log(err)
+                setError(true);
+            }
+        }
+
+    return (
+        <div className={styles.container}> 
+            <div className={styles.wrapper}>
+                <h1>Welcome, Brynn 👋 </h1>
+                <input
+                    type="text"
+                    placeholder="Username" 
+                    className={styles.input}
+                    onChange={ (e) => setUsername(e.target.value)}
+                />
+                <input
+                    placeholder="Password" 
+                    type="password"
+                    className={styles.input}
+                    onChange={ (e) => setPassword(e.target.value)}
+                />
+                <button onClick={handleClick} className={styles.button}>
+                    Sign In
+                </button>
+                {error && <span className={styles.error}>Wrong crendetials, please try again!</span>}
+            </div>
+
+        </div>
+      ); 
+}
+ 
+export default Login;
--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,24 @@
-const handler = (req, res) => {};
+const handler = (req, res) => {
+    if(req.method === "POST"){
+        const { username, password } = req.body;
+        if(
+            username === process.env.USERNAME &&
+            password === process.env.PASSWORD
+        ) {
+            res.setHeaders(
+                "Set-Cookie",
+                cookie.serialize("token", process.env.TOKEN, {
+                    MaxAge: 60 * 60,
+                    sameSite: "Strict",
+                    path: "/",
+                })
+            );
+            res.status(200).json("Logged in!");
+        } else{
+            res.status(400).json("Invalid username or password");
+        }
+    }
+};
 
 export default handler; 
 
